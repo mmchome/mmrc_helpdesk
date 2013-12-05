795dbf8612bf61835ac92503e9418bdd2b465fff
mmchome <mirimchome@gmail.com>
2013-12-05 14:42:34 +0300

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MmrcHelpdesk::Application.config.secret_key_base = 'f949e97868c9735ac0c91a79f5cfa18fe9b3262e3399929aca5333307453909904b10951967adc1d44456f79c089702d210d71a0e4c615a2477d47a44daf489f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MmrcHelpdesk::Application.config.session_store :cookie_store, key: '_mmrc_helpdesk_session'
