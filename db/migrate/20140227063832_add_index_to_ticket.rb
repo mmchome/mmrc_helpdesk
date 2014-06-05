5f3a03d8b851a007065e3a5f8cfe86812248f0a3
mmchome <mirimchome@gmail.com>
2014-06-05 14:51:45 +0300

    fix assigned_to column name

--- db/migrate/20140227063832_add_index_to_ticket.rb
+++ db/migrate/20140227063832_add_index_to_ticket.rb
@@ -1,6 +1,7 @@
 class AddIndexToTicket < ActiveRecord::Migration
   def change
   	add_index :tickets, :reported_by
+  	rename_column :tickets, :asigned_to, :assigned_to
   	add_index :tickets, :assigned_to
   end
 end
