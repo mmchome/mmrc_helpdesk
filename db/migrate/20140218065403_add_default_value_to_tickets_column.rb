f75225652941ae46c3c60e6df92e073444175ebd
mmchome <mirimchome@gmail.com>
2014-06-05 14:32:35 +0300

    fix db migration mistake

--- db/migrate/20140218065403_add_default_value_to_tickets_column.rb
+++ db/migrate/20140218065403_add_default_value_to_tickets_column.rb
@@ -1,6 +1,6 @@
 class AddDefaultValueToTicketsColumn < ActiveRecord::Migration
   def change
-  	add_column :tickets, :reported_by, :interger
+  	add_column :tickets, :reported_by, :integer
     add_column :tickets, :is_assigned, :boolean, default:false
   end
 end
