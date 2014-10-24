a2a4880c04b2153bd1c48d8b59b214a7d7181a75
mmchome <mirimchome@gmail.com>
2014-10-24 08:57:12 +0300

    Updated ticket index to add badges and color indicators

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,36 @@ module ApplicationHelper
     link_to title,params.merge(:sort => column, :direction => direction, :page => nil)
   end
 
+  def issue_type_badge(title= nil)
+  	case title
+  	when "Critical"
+  		return "-important"
+  	when "High"
+  		return "-warning"
+  	when "Medium"
+  		return "-success"
+  	when "Low"
+  		return '-info'
+  	else
+  		return ''
+  	end
+  end
+
+  def row_color ticket_state_name, assigned_to, updated_at, created_at
+  	#state is closed or cancelled
+  	if ticket_state_name == "Closed" || ticket_state_name =="Cancelled"
+  		return "inactive"
+  	#open and unassigned
+  	elsif ticket_state_name == "Open" && assigned_to==nil && created_at< (Time.now - 2.days)
+  		return "warning"
+  	elsif ticket_state_name == "Open" && assigned_to==nil && created_at>= (Time.now - 2.days)
+  		return "info"
+  	#open and not touched for a week
+  	elsif ticket_state_name == "Open" && updated_at< (Time.now - 7.days)
+  		return "error"
+  	else
+  		return "" 
+  	end
+  end
+
 end
